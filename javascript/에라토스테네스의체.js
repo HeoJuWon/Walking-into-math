ed586807982aefdabc2b7c7209127549e37567e0
HeoJuWon <podonabi@gmail.com>
2019-02-19 22:51:14 +0900

    에라토스테네스 작업중

--- javascript/에라토스테네스의체.js
+++ javascript/에라토스테네스의체.js
@@ -1,4 +1,15 @@
-function func1(self)
+function SieveOfEratosthenes()
 {
-  alert("test");
+  v = document.getElementById("eratostenes").value;
+  arr = [];
+
+  for(i = 0 ; i < v ; i++){
+      arr.add([i+2, false]);
+  }
+
+  for(i = 0 ; i < arr.size ; i++){
+    if(arr[i][0]%)
+  }
+  
+  alert(v);
 }
